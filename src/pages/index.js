9e24c0e999be70718e1b29c494019b7ad593e8e0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-10-23 14:11:13 -0400

    sort all posts from newest to oldest

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Header from '../components/header';
 
-const Layout = () => {
+const Layout = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+
   return (
     <div>
       <Header />
+      {edges.map(edge => {
+        const { frontmatter } = edge.node;
+        return (
+          <div key={frontmatter.path}>
+            {frontmatter.title}
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+export const query = graphql`
+  query HomePageQuery {
+    allMarkdownRemark(
+      sort: {
+        order: DESC,
+        fields: [frontmatter___date]
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Layout;
